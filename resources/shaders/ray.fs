04442e6e69e1ee78b2576f5ab93a7e5afd65d6c0
Rusnac-Ivan <114005933+gotoviar@users.noreply.github.com>
2022-10-17 10:08:50 +0300

    update wasm

--- resources/shaders/ray.fs
+++ resources/shaders/ray.fs
@@ -6,9 +6,14 @@ out vec4 FragColor;
 
 in vec3 FragPos;
 
+uniform vec3 uRayOrigin;
+
 const vec3 objectColor = vec3(0.0, 1.0, 1.0);
 
 void main() 
 {
-    FragColor = vec4(objectColor, 0.4);
+    float max_len_2 = 1.f;
+    float factor = smoothstep(max_len_2, 0.0, length(uRayOrigin - FragPos))/2.0;
+    
+    FragColor = vec4(objectColor, factor);
 }
