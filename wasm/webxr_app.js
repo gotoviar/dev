c88177b0fa432b2379d034a9258c39527826c2d1
Rusnac-Ivan <rusnacivan62@gmail.com>
2023-10-21 15:32:07 +0300

    update wasm

--- wasm/webxr_app.js
+++ wasm/webxr_app.js
@@ -714,17 +714,17 @@ function dbg(text) {
 }
 
 var ASM_CONSTS = {
- 782028: () => {
+ 781724: () => {
   window.shutdownGTVR();
  },
- 782051: $0 => {
+ 781747: $0 => {
   window.location.reload();
   window.open(UTF8ToString($0), "_self").focus();
  },
- 782129: () => {
+ 781825: () => {
   window.shutdownGTVR();
  },
- 782152: () => {
+ 781848: () => {
   window.location.reload();
  }
 };
